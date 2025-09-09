cf6b7ce2c2fb7c42cb213f72393f4a42d5393ff7
João Victor <joao.vsholz@aluno.edu.es.gov.br>
2025-09-09 11:55:01 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>aula 03-PHP</title>
+    </head>
+    
+    <body>
+        <?php
+            $nome="joão Victor";
+            
+            $n1=10;
+            $n2=2;
+
+            $soma=$n1+$n2;
+
+            echo "Meu nome é " .$nome;
+            echo "<br> Meu nome é $nome" ;
+
+            echo "<br><br>A soma de $n1 com $n2 é igual a $soma.";
+        ?>
+    </body>
+</html> 
