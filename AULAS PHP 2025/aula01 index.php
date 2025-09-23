c5553dd3313eb85169b7da6e8a83ab59fb9c27c3
João Victor <joao.vsholz@aluno.edu.es.gov.br>
2025-09-23 11:47:36 -0300

    Create aula01 index.php

---
+++ AULAS PHP 2025/aula01 index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Aula 01 PHP</title>
+    </head>
+    <body>
+        <?php
+            echo"Olá, Mundo!";
+            echo"joão victor";
+        ?>
+    </body>
+</html>
