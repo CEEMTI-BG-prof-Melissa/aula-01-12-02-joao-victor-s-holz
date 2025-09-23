c6189b9c03bb85a2261d9b3969944c42775ef798
João Victor <joao.vsholz@aluno.edu.es.gov.br>
2025-09-23 11:48:13 -0300

    Create aula02 index.php

---
+++ AULAS PHP 2025/aula02 index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>aula 02-PHP</title>
+    </head>
+    
+    <body>
+        <?php
+            $nome="joão Victor";
+
+            echo "Meu nome é " .$nome;
+        ?>
+    </body>
+</html> 
