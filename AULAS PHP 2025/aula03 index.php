bae5aa2ec464fe48d1fc27cc8abea8e0c1e9986c
João Victor <joao.vsholz@aluno.edu.es.gov.br>
2025-09-23 11:48:50 -0300

    Create aula03 index.php

---
+++ AULAS PHP 2025/aula03 index.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <title>aula 03-PHP</title>
+    </head>
+    
+    <body>
+        <?php
+            $nome="joão Victor";
+            
+            $n1=10;
+            $n2=2;
+
+            $soma=$n1+$n2;
+
+            echo "Meu nome é " .$nome;
+            echo "<br> Meu nome é $nome" ;
+
+            echo "<br><br>A soma de $n1 com $n2 é igual a $soma.";
+
+            echo "<br><br> A subtração dos números $n1 e $n2 é igual a: " .($n1-$n2).".";
+
+            echo "<br><br> A multiplicação dos números $n1 e $n2 é igual a: ".($n1*$n2).".";
+
+            echo "<br><br> A divisão dos números $n1 e $n2 é igual a: ".($n1/$n2).".";
+
+            echo "<br><br> O resto da divisão dos números $n1 e $n2 é igual a: ".($n1%$n2).".";
+        ?>
+    </body>
+</html> 
