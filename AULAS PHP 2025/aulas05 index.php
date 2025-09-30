fa7d00835721d0dff5963668e961dd4409e7e09d
João Victor <joao.vsholz@aluno.edu.es.gov.br>
2025-09-30 11:44:18 -0300

    Update aulas05 index.php

--- AULAS PHP 2025/aulas05 index.php
+++ AULAS PHP 2025/aulas05 index.php
@@ -11,6 +11,19 @@
             echo "O valor absoluto de $n1 é" .abs($n1);
             echo "<br> O número $n2 <sup>$n1</sup> é igual a " .pow($n2, $n1);
             echo "<br> A raiz quadrada de $n2 é igual a " .sqrt($n2);
+            echo "<br> <br>";
+
+            echo "<br> O valor arredondado de $n2 é igual a " .round($n2);
+            echo "<br> O valor arredondado de $n2 é igual a " .ceil($n2);
+            echo "<br> O valor arredondado de $n2 é igual a " .floor($n2);
+            echo "<br> <br>";
+
+            echo "A parte inteira de $n2 é igual " . intval($n2);
+            echo "<br> <br>";
+
+            echo "Um pirulito custa R$" .number_format($n1, 2, ",", ".");
+
+
 
             ?>
     </body>
